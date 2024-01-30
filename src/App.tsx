c73a30c7ef4f17d57bb26767b6c6ecd7528c0429
Lefia <lefia1222@gmail.com>
2024-01-31 00:03:40 +0800

    change some style

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import { invoke } from "@tauri-apps/api/tauri";
 import NavBar from "./components/NavBar";
 
 function App() {
   
   return(
-    <div>
+    <>
       <NavBar/>
-      <div className="h-[92vh] w-full flex justify-center">
-        <div className="h-full w-[75%] px-[5%] bg-slate-200 flex flex-col">
-          <div className="h-auto w-full mb-[2%] mt-[7%]">
-            <h1 className="introColor font-bold text-[4vh]">Choose the correct answer to fill in:</h1>
+      <div 
+        className={`
+          mx-auto md:max-w-[900px] w-full
+          bg-white h-screen flex justify-center
+        `}
+      >
+          <div className='pt-8'>
+            <h1>Choose the correct answer to fill in:</h1>
+            <h1>This line is question</h1>
           </div>
-          <div className="h-auto w-full mb-[2%]">
-            <h1 className="text-black font-bold text-[3vh]">This line is question</h1>
-          </div>
-        </div>
       </div>
-    </div>
+    </>
   )
 }
 
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,17 +1,27 @@
 // TODO: button onclick change status
 function NavBar() {
     return (
-      <nav className="z-1000 w-[100%] h-[8vh] flex justify-between items-center bg-slate-900 shadow-xl">
-        <div className="bg-slate-900 w-1/4 h-full flex justify-start pl-[5%]">
-          <button type="button" className="flex items-center justify-center font-bold ">
-            <h1 className="text-white text-[2vw]">Vocab Daily</h1>
+      <nav className="md:h-16 h-12 md:pt-2 bg-slate-900">
+        <div 
+          className={`
+            w-full max-w-[900px] mx-auto flex justify-between items-center
+            md:text-lg text-md text-white font-bold
+          `}
+        >
+          <button 
+            type="button" 
+            className="px-4 py-2"
+          >
+            <h1>Vocab Daily</h1>
+          </button>
+          <button 
+            type="button" 
+            className="px-4 py-2"
+          >
+            <h1>About us</h1>
           </button>
         </div>
-        <div className="bg-slate-900 w-1/6 h-full flex justify-end pr-[5%]">
-          <button type="button" className="flex items-center justify-center font-bold ">
-            <h1 className="text-white text-[1.5vw]">About us</h1>
-          </button>
-        </div>
+
       </nav>
     )
   }
